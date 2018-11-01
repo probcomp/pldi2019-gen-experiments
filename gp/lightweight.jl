ef61c1f4a8516aca0ecb890320a8cdda418e6637
Feras A Saad <fsaad@mit.edu>
2018-11-01 16:35:27 -0400

    Minor plotting.

--- gp/lightweight.jl
+++ gp/lightweight.jl
@@ -136,8 +136,7 @@ function experiment()
         ax[:plot](new_xs, new_ys, color="red")
 
         ax[:set_xlim]((0, 1.5))
-        ax[:set_ylim]((-1, 1))
-        ax[:grid](true)
+        ax[:set_ylim]((-1.5, 1.5))
     end
     fig[:set_tight_layout](true)
     fig[:set_size_inches](20, 14)
