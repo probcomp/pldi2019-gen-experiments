3ca9a1966b4ab73144a178baac5b51d59116fc75
Feras A Saad <fsaad@mit.edu>
2018-11-13 19:52:18 -0500

    Let summarize take a list.

--- regression/summarize.sh
+++ regression/summarize.sh
@@ -2,8 +2,15 @@
 
 set -u
 
-fname=${1}
-iters=$(cat ${fname}| head -n1 | cut -d, -f1)
-median=$(datamash -t, median 2 < ${fname})
-iqr=$(datamash -t, iqr 2 < ${fname})
-echo $(python -c "print 1000*${median}/${iters}, 1000*${iqr}/${iters}")
+command -v datamash >/dev/null 2>&1 || {
+    echo >&2 "Install datamash using apt-get, please."; exit 1;
+}
+
+fnames="${@}"
+
+for fname in ${fnames}; do
+    iters=$(cat ${fname}| head -n1 | cut -d, -f1)
+    median=$(datamash -t, median 2 < ${fname})
+    iqr=$(datamash -t, iqr 2 < ${fname})
+    echo ${fname} $(python -c "print 1000*${median}/${iters}, 1000*${iqr}/${iters}")
+done
