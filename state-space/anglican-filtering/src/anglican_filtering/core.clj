85109265ff74bc2cf63191f17303926412dcc73d
Marco Cusumano-Towner <imarcoam@gmail.com>
2019-03-14 19:53:16 -0400

    checkpint on anglican experiment

--- state-space/anglican-filtering/src/anglican_filtering/core.clj
+++ state-space/anglican-filtering/src/anglican_filtering/core.clj
@@ -1,6 +1,7 @@
 (ns anglican-filtering.core
   (:use clojure.repl
-        [anglican core runtime emit]))
+        [anglican core runtime emit])
+  (:require [clojure.data.json :as json]))
 
 (def prior [0.2 0.3 0.5])
 
@@ -30,11 +31,27 @@
 (defn run-particle-filter
   "Run a particle filter and return the marginal likelihood estimate"
   [number-of-particles]
-    (first (doquery :smc hmm-unrolled [] :number-of-particles number-of-particles)))
+    (let [start (System/nanoTime)
+          lml ((first (doquery :smc hmm [] :number-of-particles number-of-particles))
+               :log-weight)
+          elapsed (double (/ (- (System/nanoTime) start) 1e9))]
+         {:lml lml :elapsed elapsed}))
 
 (defn filtering-experiment
     []
-    (println (run-particle-filter 100000)))
+    (let [num-particles-list [1 2 3 5 7 10 15 20 25 30 35 40 45 50 60 70 80 90 100 200 300]
+          num-reps 50]
+        (println (spit "anglican-results.json" (json/write-str (zipmap num-particles-list (mapv
+            (fn [num-particles]
+                (let 
+                    ; results is a vector of {:lml lml :elapsed elapsed}
+                    [results (mapv (fn [i] (run-particle-filter num-particles))
+                                   (range num-reps))]
+                    {:lmls (mapv (fn [res] (res :lml))
+                                 results)
+                     :elapsed (mapv (fn [res] (res :elapsed))
+                                    results)}))
+            num-particles-list)))))))
 
 ;; see:
 ;; https://bitbucket.org/probprog/anglican-user
--- state-space/plot_filtering_results.jl
+++ state-space/plot_filtering_results.jl
@@ -1,7 +1,7 @@
 using Statistics: median, mean, std
 using PyPlot
-using JLD
 using Printf: @sprintf
+import JSON
 
 function print_runtimes(num_particles_list::Vector{Int}, results::Dict, label::String)
     median_times = [median(results[num_particles][2]) for num_particles in num_particles_list]
@@ -12,20 +12,50 @@ function print_runtimes(num_particles_list::Vector{Int}, results::Dict, label::S
     end
 end
 
-function plot_results(num_particles_list::Vector{Int}, results::Dict, label::String, color::String)
-    median_times = [median(results[num_particles][2]) for num_particles in num_particles_list]
-    stdev_times = [std(results[num_particles][2]) for num_particles in num_particles_list]
-    mean_lmls = [mean(results[num_particles][1]) for num_particles in num_particles_list]
-    stdev_lmls = [std(results[num_particles][1]) for num_particles in num_particles_list]
+function plot_results(num_particles_list::Vector{Int}, results::Dict, label::String,
+            color::String, linestyle="-")
+    println(keys(results))
+    median_times = [median(results[string(num_particles)]["elapsed"]) for num_particles in num_particles_list]
+    stdev_times = [std(results[string(num_particles)]["elapsed"]) for num_particles in num_particles_list]
+    mean_lmls = [mean(results[string(num_particles)]["lmls"]) for num_particles in num_particles_list]
+    stdev_lmls = [std(results[string(num_particles)]["lmls"]) for num_particles in num_particles_list]
     println("min: $(minimum(mean_lmls)))")
     println("max: $(maximum(mean_lmls)))")
     plot(median_times, mean_lmls, 
 	    color=color,
-	    label=label)
+	    label=label,
+        linestyle=linestyle)
 end
 
-results = load("results.jld")
-results_turing = load("../turing-planning/results_turing.jld")
+anglican_results = JSON.parsefile("anglican-filtering/anglican-results.json") # TODO this is for the wrong HMM.
+venture_results = JSON.parsefile("venture/venture_results.json")
+turing_results = JSON.parsefile("turing/turing_results.json")
+gen_results_static_default_proposal = JSON.parsefile("gen/gen_results_static_default_proposal.json")
+gen_results_lightweight_unfold_custom_proposal = JSON.parsefile("gen/gen_results_lightweight_unfold_custom_proposal.json")
+gen_results_lightweight_unfold_default_proposal = JSON.parsefile("gen/gen_results_lightweight_unfold_default_proposal.json")
+gen_results_lightweight_custom_proposal = JSON.parsefile("gen/gen_results_lightweight_custom_proposal.json")
+gen_results_lightweight_default_proposal = JSON.parsefile("gen/gen_results_lightweight_default_proposal.json")
+gen_results_static_custom_proposal = JSON.parsefile("gen/gen_results_static_custom_proposal.json")
+
+# plot time accuracy curve
+const turing_num_particles_list = [1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 200, 300]
+const gen_num_particles_list = [1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 200, 300]
+const venture_num_particles_list = [1, 3, 10, 30, 100]
+const anglican_num_particles_list = [1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 200, 300]
+figure(figsize=(8,4))
+plot_results(anglican_num_particles_list, anglican_results, "Anglican", "blue")
+plot_results(venture_num_particles_list, venture_results, "Venture", "green")
+plot_results(turing_num_particles_list, turing_results, "Turing", "purple")
+plot_results(gen_num_particles_list, gen_results_lightweight_unfold_default_proposal, "Gen (Default Proposal)", "red", "--")
+plot_results(gen_num_particles_list, gen_results_lightweight_unfold_custom_proposal, "Gen (Custom Proposal)", "orange", "--")
+legend(loc="lower right")
+ylabel("Accuracy (LML estimate)")
+xlabel("seconds")
+gca()[:set_xscale]("log")
+tight_layout()
+savefig("lml_estimates.pdf")
+
+exit()
 
 # experiments with static model
 results_static_default_proposal = results["results_static_default_proposal"]
@@ -39,11 +69,14 @@ results_lightweight_custom_proposal = results["results_lightweight_custom_propos
 results_lightweight_unfold_default_proposal = results["results_lightweight_unfold_default_proposal"]
 results_lightweight_unfold_custom_proposal = results["results_lightweight_unfold_custom_proposal"]
 
-# Turing.jl
-results_turing = results_turing["results_turing"]
+# experiments with Turing.jl
+results_turing = results["results_turing"]
 
-const num_particles_list_default = [20, 30, 50, 70, 100, 200, 300]
+#const num_particles_list_default = [20, 30, 50, 70, 100, 200, 300]
 const num_particles_list_custom = [1, 2, 3, 5, 7, 10, 20, 30, 50, 70, 100, 200, 300]
+const num_particles_list_default = num_particles_list_custom
+num_particles_list_turing = copy(num_particles_list_custom)
+push!(num_particles_list_turing, 1000)
 
 #######################
 # print runtime table #
@@ -68,6 +101,7 @@ print_runtimes([100], results_lightweight_unfold_default_proposal, "Flexible DSL
 figure(figsize=(4,2))
 plot_results(num_particles_list_custom, results_static_custom_proposal, "Custom Proposal", "orange")
 plot_results(num_particles_list_default, results_static_default_proposal, "Default Proposal", "blue")
+plot_results(num_particles_list_default, results_turing, "Turing", "red")
 legend(loc="lower right")
 ylabel("Accuracy (LML estimate)")
 xlabel("seconds")
