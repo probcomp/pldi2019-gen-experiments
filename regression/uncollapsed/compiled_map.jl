37b1be9cf3c006b7a4e68206c98c84d1f3a9a24b
Feras A Saad <fsaad@mit.edu>
2019-03-19 17:07:34 -0400

    uncollapsed/compiled_mh.jl is running.

--- regression/uncollapsed/compiled_map.jl
+++ regression/uncollapsed/compiled_map.jl
@@ -3,90 +3,39 @@ include("../shared.jl")
 using Gen
 import Random
 
-using FunctionalCollections
-using ReverseDiff
-
-############################
-# reverse mode AD for fill #
-############################
-
-function Base.fill(x::ReverseDiff.TrackedReal{V,D,O}, n::Integer) where {V,D,O}
-    tp = ReverseDiff.tape(x)
-    out = ReverseDiff.track(fill(ReverseDiff.value(x), n), V, tp)
-    ReverseDiff.record!(tp, ReverseDiff.SpecialInstruction, fill, (x, n), out)
-    return out
-end
-
-@noinline function ReverseDiff.special_reverse_exec!(
-        instruction::ReverseDiff.SpecialInstruction{typeof(fill)})
-    x, n = instruction.input
-    output = instruction.output
-    ReverseDiff.istracked(x) &&
-        ReverseDiff.increment_deriv!(x, sum(ReverseDiff.deriv(output)))
-    ReverseDiff.unseed!(output)
-    return nothing
-end
-
-@noinline function ReverseDiff.special_forward_exec!(
-        instruction::ReverseDiff.SpecialInstruction{typeof(fill)})
-    x, n = instruction.input
-    ReverseDiff.value!(instruction.output, fill(ReverseDiff.value(x), n))
-    return nothing
-end
-
 #########
 # model #
 #########
 
-@compiled @gen function datum(x::Float64, @ad(inlier_std::Float64),
-        @ad(outlier_std::Float64), @ad(slope::Float64), @ad(intercept::Float64))
-    is_outlier::Bool = @addr(bernoulli(0.5), :z)
-    std::Float64 = is_outlier ? inlier_std : outlier_std
-    y::Float64 = @addr(normal(x * slope + intercept, sqrt(exp(std))), :y)
+@gen (static) function datum(x::Float64, (grad)(inlier_std::Float64),
+        (grad)(outlier_std::Float64), (grad)(slope::Float64),
+        (grad)(intercept::Float64))
+    is_outlier = @trace(bernoulli(0.5), :z)
+    std = is_outlier ? inlier_std : outlier_std
+    y = @trace(normal(x * slope + intercept, sqrt(exp(std))), :y)
     return y
 end
 
-data = plate(datum)
+data = Map(datum)
 
-function compute_argdiff(inlier_std_diff, outlier_std_diff, slope_diff, intercept_diff)
-    if all([c == NoChoiceDiff() for c in [
-            inlier_std_diff, outlier_std_diff, slope_diff, intercept_diff]])
-        noargdiff
-    else
-        unknownargdiff
-    end
-end
-
-@compiled @gen function model(xs::Vector{Float64})
-    n::Int = length(xs)
-    inlier_std::Float64 = @addr(normal(0, 2), :inlier_std)
-    outlier_std::Float64 = @addr(normal(0, 2), :outlier_std)
-    slope::Float64 = @addr(normal(0, 2), :slope)
-    intercept::Float64 = @addr(normal(0, 2), :intercept)
-    inlier_std_diff::Union{PrevChoiceDiff{Float64},NoChoiceDiff} = @change(:inlier_std)
-    outlier_std_diff::Union{PrevChoiceDiff{Float64},NoChoiceDiff} = @change(:outlier_std)
-    slope_diff::Union{PrevChoiceDiff{Float64},NoChoiceDiff} = @change(:slope)
-    intercept_diff::Union{PrevChoiceDiff{Float64},NoChoiceDiff} = @change(:intercept)
-    argdiff::Union{NoArgDiff,UnknownArgDiff} = compute_argdiff(
-        inlier_std_diff, outlier_std_diff, slope_diff, intercept_diff)
-    @addr(data(xs, fill(inlier_std, n), fill(outlier_std, n),
-               fill(slope, n), fill(intercept, n)),
-          :data, argdiff)
+@gen (static) function model(xs::Vector{Float64})
+    n = length(xs)
+    inlier_std = @trace(normal(0, 2), :inlier_std)
+    outlier_std = @trace(normal(0, 2), :outlier_std)
+    slope = @trace(normal(0, 2), :slope)
+    intercept = @trace(normal(0, 2), :intercept)
+    @trace(data(xs, fill(inlier_std, n), fill(outlier_std, n),
+            fill(slope, n), fill(intercept, n)),
+        :data)
 end
 
 #######################
 # inference operators #
 #######################
 
-@compiled @gen function flip_z(z::Bool)
-    @addr(bernoulli(z ? 0.0 : 1.0), :z)
-end
-
-data_proposal = at_dynamic(flip_z, Int)
-
-@compiled @gen function is_outlier_proposal(prev, i::Int)
-    prev_z::Bool = get_assignment(prev)[:data => i => :z]
-    @addr(data_proposal(i, (prev_z,)), :data)
+@gen (static) function is_outlier_proposal(prev, i::Int)
+    prev_z = prev[:data => i => :z]
+    @trace(bernoulli(prev_z ? 0.0 : 1.0), :data => i => :z)
 end
 
 Gen.load_generated_functions()
@@ -95,18 +44,9 @@ Gen.load_generated_functions()
 # run experiment #
 ##################
 
-selection = let
-    s = DynamicAddressSet()
-    push!(s, :slope)
-    push!(s, :intercept)
-    push!(s, :inlier_std)
-    push!(s, :outlier_std)
-    StaticAddressSet(s)
-end
-
 function do_inference(n)
     (xs, ys) = load_dataset("../train.csv")
-    observations = DynamicAssignment()
+    observations = choicemap()
     for (i, y) in enumerate(ys)
         observations[:data => i => :y] = y
     end
@@ -125,36 +65,35 @@ function do_inference(n)
         # 1. Use adaptive gradients 1e-1 to 1e-10
         # 2. Make selection for slope/intercept and inlier_std/outlier_std.
         # 3. Make 10 steps instead of 1 step.
-        trace = map_optimize(model, selection, trace,
+        trace = map_optimize(trace,
+            select(:slope, :intercept, :inlier_std, :outlier_std),
             max_step_size=1e-6, min_step_size=1e-6)
 
         # step on the outliers
         for j=1:length(xs)
-            trace = mh(model, is_outlier_proposal, (j,), trace)
+            (trace, _accept) = mh(trace, is_outlier_proposal, (j,))
         end
         elapsed = time() - start
         runtime += elapsed
 
         # report loop stats
-        score = get_call_record(trace).score
-        assignment = get_assignment(trace)
+        score = get_score(trace)
         println((score,
-            assignment[:slope],
-            assignment[:intercept],
-            sqrt(exp(assignment[:inlier_std])),
-            sqrt(exp(assignment[:outlier_std]))))
+            trace[:slope],
+            trace[:intercept],
+            sqrt(exp(trace[:inlier_std])),
+            sqrt(exp(trace[:outlier_std]))))
     end
 
-    score = get_call_record(trace).score
-    assignment = get_assignment(trace)
+    score = get_score(trace)
     return (
         n,
         runtime,
         score,
-        assignment[:slope],
-        assignment[:intercept],
-        assignment[:inlier_std],
-        assignment[:outlier_std])
+        trace[:slope],
+        trace[:intercept],
+        trace[:inlier_std],
+        trace[:outlier_std])
 end
 
 #################
