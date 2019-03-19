bb62efce6d44c8c1fc5a74812bf564ddf86ec17c
Feras A Saad <fsaad@mit.edu>
2019-03-19 17:33:07 -0400

    uncollapsed/lightweight_mala.jl is running.

--- regression/uncollapsed/lightweight_mala.jl
+++ regression/uncollapsed/lightweight_mala.jl
@@ -4,37 +4,19 @@ include("../shared.jl")
 # model #
 #########
 
-@gen function datum(x::Float64, @ad(inlier_std), @ad(outlier_std), @ad(slope), @ad(intercept))
-    is_outlier = @addr(bernoulli(0.5), :z)
-    std = is_outlier ? inlier_std : outlier_std
-    y = @addr(normal(x * slope + intercept, sqrt(exp(std))), :y)
-    return y
-end
-
-data = plate(datum)
-
 @gen function model(xs::Vector{Float64})
-    n = length(xs)
-    inlier_std = exp(@addr(normal(0, 2), :inlier_std))
-    outlier_std = exp(@addr(normal(0, 2), :outlier_std))
-    slope = @addr(normal(0, 2), :slope)
-    intercept = @addr(normal(0, 2), :intercept)
-    @diff begin
-        argdiff = noargdiff
-        for addr in [:slope, :intercept, :inlier_std, :outlier_std]
-            if !isnodiff(@choicediff(addr))
-                argdiff = unknownargdiff
-            end
-        end
+    inlier_std = @trace(normal(0, 2), :inlier_std)
+    outlier_std = @trace(normal(0, 2), :outlier_std)
+    slope = @trace(normal(0, 2), :slope)
+    intercept = @trace(normal(0, 2), :intercept)
+    ys = Float64[]
+    for (i, x) in enumerate(xs)
+        is_outlier = @trace(bernoulli(.5), :data => i => :z)
+        std = is_outlier ? inlier_std : outlier_std
+        mean = x * slope + intercept
+        y = @trace(normal(mean, sqrt(exp(std))), :data => i => :y)
+        push!(ys, y)
     end
-    ys = @addr(
-        data(
-            xs,
-            fill(inlier_std, n),
-            fill(outlier_std, n),
-            fill(slope, n),
-            fill(intercept, n)),
-        :data, argdiff)
     return ys
 end
 
@@ -43,31 +25,20 @@ end
 #######################
 
 @gen function is_outlier_proposal(prev, i::Int)
-    prev_z = get_assignment(prev)[:data => i => :z]
-    @addr(bernoulli(prev_z ? 0.0 : 1.0), :data => i => :z)
+    z = prev[:data => i => :z]
+    @trace(bernoulli(z ? 0.0 : 1.0), :data => i => :z)
 end
-
-@gen function observer(ys::Vector{Float64})
-    for (i, y) in enumerate(ys)
-        @addr(dirac(y), :data => i => :y)
-    end
-end
-
-Gen.load_generated_functions()
 
 ##################
 # run experiment #
 ##################
 
-selection = DynamicAddressSet()
-push_leaf_node!(selection, :slope)
-push_leaf_node!(selection, :intercept)
-push_leaf_node!(selection, :inlier_std)
-push_leaf_node!(selection, :outlier_std)
-
 function do_inference(method, n)
-    xs, ys = generate_dataset()
-    observations = get_assignment(simulate(observer, (ys,)))
+    (xs, ys) = load_dataset("../train.csv")
+    observations = choicemap()
+    for (i, y) in enumerate(ys)
+        observations[:data => i => :y] = y
+    end
 
     # initial trace
     (trace, _) = generate(model, (xs,), observations)
@@ -76,43 +47,46 @@ function do_inference(method, n)
     for i=1:n
         start  = time()
         # step on the numeric parameters.
+        selection = select(:slope, :intercept, :inlier_std, :outlier_std)
         if method == "mala"
-            trace = mala(model, selection, trace, 0.0001)
+            (trace, _accept) = mala(trace, selection, 0.0001)
         elseif method == "hmc"
-            trace = hmc(model, selection, trace)
+            (trace, _accept) = hmc(trace, selection)
         else
             @assert false "Unknown method: $(method)"
         end
 
         # step on the outliers
         for j=1:length(xs)
-            trace = mh(model, is_outlier_proposal, (j,), trace)
+            (trace, _accept) = mh(trace, is_outlier_proposal, (j,))
         end
         elapsed = time() - start
         runtime += elapsed
 
-        score = get_call_record(trace).score
-        assignment = get_assignment(trace)
+        score = get_score(trace)
         println((
             ("score", score),
-            ("slope", assignment[:slope]),
-            ("intercept", assignment[:intercept]),
-            ("inlier_std", sqrt(exp(assignment[:inlier_std]))),
-            ("outlier_std", sqrt(exp(assignment[:outlier_std]))),
+            ("slope", trace[:slope]),
+            ("intercept", trace[:intercept]),
+            ("inlier_std", sqrt(exp(trace[:inlier_std]))),
+            ("outlier_std", sqrt(exp(trace[:outlier_std]))),
         ))
     end
-    assignment = get_assignment(trace)
-    score = get_call_record(trace).score
+    score = get_score(trace)
     return (
         n,
         runtime,
         score,
-        assignment[:slope],
-        assignment[:intercept],
-        assignment[:inlier_std],
-        assignment[:outlier_std]
+        trace[:slope],
+        trace[:intercept],
+        trace[:inlier_std],
+        trace[:outlier_std]
     )
 end
+
+#################
+# run inference #
+#################
 
 do_inference("mala", 10)
 
