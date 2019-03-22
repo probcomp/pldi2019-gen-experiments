4d0fa4249a6e12593e17fda35026830eac59f231
Marco Cusumano-Towner <imarcoam@gmail.com>
2019-03-22 19:34:30 -0400

    remove dead plotting code

--- state-space/plot_filtering_results.jl
+++ state-space/plot_filtering_results.jl
@@ -27,7 +27,7 @@ function plot_results(num_particles_list::Vector{Int}, results::Dict, label::Str
         linestyle=linestyle)
 end
 
-anglican_results = JSON.parsefile("anglican-filtering/anglican-results.json")
+anglican_results = JSON.parsefile("anglican/anglican-results.json")
 venture_results = JSON.parsefile("venture/venture_results.json")
 turing_results = JSON.parsefile("turing/turing_results.json")
 gen_results_static_default_proposal = JSON.parsefile("gen/gen_results_static_default_proposal.json")
@@ -52,59 +52,7 @@ legend(loc="lower right")
 ylabel("Accuracy (LML estimate)")
 xlabel("seconds")
 gca()[:set_xscale]("log")
-tight_layout()
-savefig("lml_estimates.pdf")
-
-exit()
-
-# experiments with static model
-results_static_default_proposal = results["results_static_default_proposal"]
-results_static_custom_proposal = results["results_static_custom_proposal"]
-
-# experiments with lightweight model (no unfold)
-results_lightweight_default_proposal = results["results_lightweight_default_proposal"]
-results_lightweight_custom_proposal = results["results_lightweight_custom_proposal"]
-
-# experiments with unfold
-results_lightweight_unfold_default_proposal = results["results_lightweight_unfold_default_proposal"]
-results_lightweight_unfold_custom_proposal = results["results_lightweight_unfold_custom_proposal"]
-
-# experiments with Turing.jl
-results_turing = results["results_turing"]
-
-#const num_particles_list_default = [20, 30, 50, 70, 100, 200, 300]
-const num_particles_list_custom = [1, 2, 3, 5, 7, 10, 20, 30, 50, 70, 100, 200, 300]
-const num_particles_list_default = num_particles_list_custom
-num_particles_list_turing = copy(num_particles_list_custom)
-push!(num_particles_list_turing, 1000)
-
-#######################
-# print runtime table #
-#######################
-
-print_runtimes([100], results_turing, "Turing")
-print_runtimes([100], results_static_default_proposal, "Restricted DSL  + unfold (default proposal)")
-print_runtimes([100], results_lightweight_default_proposal, "Flexible DSL (default proposal)")
-print_runtimes([100], results_lightweight_unfold_default_proposal, "Flexible DSL + unfold (default proposal)")
-
-# Turing, 100 particles: 0.306 +/- 0.153
-# Restricted DSL  + unfold (default proposal), 100 particles: 0.013 +/- 0.002
-# Flexible DSL (default proposal), 100 particles: 0.926 +/- 0.066
-#Flexible DSL + unfold (default proposal), 100 particles: 0.078 +/- 0.007
-
-##################
-# generate plots #
-##################
-
-# plot of all data
-
-figure(figsize=(4,2))
-plot_results(num_particles_list_custom, results_static_custom_proposal, "Custom Proposal", "orange")
-plot_results(num_particles_list_default, results_static_default_proposal, "Default Proposal", "blue")
-plot_results(num_particles_list_default, results_turing, "Turing", "red")
-legend(loc="lower right")
-ylabel("Accuracy (LML estimate)")
-xlabel("seconds")
-gca()[:set_xscale]("log")
+ax = gca()
+ax.set_ylim((0, 60))
 tight_layout()
 savefig("lml_estimates.pdf")
