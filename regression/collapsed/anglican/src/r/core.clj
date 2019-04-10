84d9d7711cbe8eaaeb34bc548600744d028ecf9b
Feras A Saad <fsaad@mit.edu>
2019-04-10 15:02:34 -0400

    Fix steps and num steps.

--- regression/collapsed/anglican/src/r/core.clj
+++ regression/collapsed/anglican/src/r/core.clj
@@ -134,12 +134,12 @@
       (println (str i "," (* (inc i) (:per-step result)) "," (:slope trace) "," (:intercept trace) "," (:inlier-std-choice trace) ","
         (:outlier-std-choice trace))))))
 
-(def num-steps 2000)
+(def num-steps 200)
 (def num-experiments 1)
 (defn experiment []
   (doseq [i (range num-experiments)]
     ;(println (str "Lightweight MH for " n-steps " steps (collapsed) - iterate " i))
-    (println "iters,time-in-ms,slope,intercept,inlier-std,outlier-std")
+    (println "num_steps,runtime,score,slope,intercept,inlier_log_var,outlier_log_var")
     (process-lmh-to-csv-rows num-steps
       (run-lmh regress-collapsed regress-collapsed-score num-steps))))
   ; (println "Lightweight MH for 1000 steps (collapsed)")
