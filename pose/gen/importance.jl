07ee6071ebd8902aee0088891872b53d5cd1f4f5
Marco Cusumano-Towner <imarcoam@gmail.com>
2019-04-13 01:06:51 -0400

    pose importance

--- pose/gen/importance.jl
+++ pose/gen/importance.jl
@@ -19,31 +19,33 @@ trace = simulate(generative_model, ())
 image = trace[:image]
 (ground_truth_latent_image, _, _) = get_retval(trace)
 
+const n = 10
+
 # run sampling importance resampling with the trained proposal
 function custom_proposal_importance_resampling()
     constraints = choicemap((:image, image))
     (inferred_trace, _) = importance_resampling(
         generative_model, (), constraints,
-        proposal, (image,), 1000)
+        proposal, (image,), 1)
     (latent_image, _, _) = get_retval(inferred_trace)
     latent_image
 end
 
-custom_proposal_latent_images = [custom_proposal_importance_resampling() for _=1:4]
+custom_proposal_latent_images = [custom_proposal_importance_resampling() for _=1:n]
 
 # run sampling importance resampling with the prior proposal
 function generic_proposal_importance_resampling()
     constraints = choicemap((:image, image))
     (inferred_trace, _) = importance_resampling(
-        generative_model, (), constraints, 1000)
+        generative_model, (), constraints, 1)
     (latent_image, _, _) = get_retval(inferred_trace)
     latent_image
 end
 
-generic_proposal_latent_images = [generic_proposal_importance_resampling() for _=1:4]
+generic_proposal_latent_images = [generic_proposal_importance_resampling() for _=1:n]
 
 # show the observed image and the reconstruction
-top_row = hcat(ground_truth_latent_image, image, zero(image), zero(image))
+top_row = hcat(ground_truth_latent_image, image, [zero(image) for i=1:(n-2)]...)
 middle_row = hcat(generic_proposal_latent_images...)
 bottom_row = hcat(custom_proposal_latent_images...)
 combined = vcat(top_row, middle_row, bottom_row)
